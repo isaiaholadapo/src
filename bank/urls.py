7d9e1d68adec7db5619eaf3569f312367bed075c
Isaiah <isaiaholadapo18@gmail.com>
2021-02-22 14:50:27 +0100

    added user registration

---
+++ bank/urls.py
@@ -0,0 +1,33 @@
+"""bank URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url
+from django.urls import path
+from django.contrib import admin
+
+from account.views import (
+    home_view,
+    registration_view,
+    login_view,
+    logout_view,
+)
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    path('', home_view, name = 'home'),
+    path('register/', registration_view, name = 'register'),
+    path('login/', login_view, name = 'login'),
+    path('logout/',  logout_view, name = 'logout')
+]
